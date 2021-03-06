b609c8c45bad7f04aa7a06eda5e8bb1c008c2e06
vetsinen <vetsinen@gmail.com>
2021-03-06 15:23:00 +0200

    works parties and studios page

---
+++ fragments/studios.js
@@ -0,0 +1,39 @@
+s = [{
+    "id": 1,
+    "shortName": "Kiev Latino",
+    "link": "n/a",
+    "description": "танцевальная студия\n\nНазвания: Киев Латино\n\nУслуги: латиноамериканские танцы\n\nГород: Киев, Украина\n\nРайон: Голосеевский\n\nМетро: Дворец «Украина»\n\nАдрес: ул. Предславинськая, 12\n\nТелефон: (067) 597-03-78",
+    "created_at": "2021-03-06T13:10:24.865Z",
+    "updated_at": "2021-03-06T13:10:24.886Z",
+    "logo": {
+        "id": 7,
+        "name": "kiev latino.jpg",
+        "alternativeText": "",
+        "caption": "",
+        "width": 300,
+        "height": 225,
+        "formats": {
+            "thumbnail": {
+                "name": "thumbnail_kiev latino.jpg",
+                "hash": "thumbnail_kiev_latino_bdc21fb48c",
+                "ext": ".jpg",
+                "mime": "image/jpeg",
+                "width": 208,
+                "height": 156,
+                "size": 7.69,
+                "path": null,
+                "url": "/uploads/thumbnail_kiev_latino_bdc21fb48c.jpg"
+            }
+        },
+        "hash": "kiev_latino_bdc21fb48c",
+        "ext": ".jpg",
+        "mime": "image/jpeg",
+        "size": 13.74,
+        "url": "/uploads/kiev_latino_bdc21fb48c.jpg",
+        "previewUrl": null,
+        "provider": "local",
+        "provider_metadata": null,
+        "created_at": "2021-03-06T13:10:18.999Z",
+        "updated_at": "2021-03-06T13:10:19.022Z"
+    }
+}]
